9ce3d45a2725067d3454db33db53c1d35e92163f
Krishna Venkatachala <Krishna.Venkatachala@outlook.com>
2021-05-20 20:27:19 +0530

    changed the result text

--- contactform.php
+++ contactform.php
@@ -29,7 +29,7 @@
 		// If there are no errors, send the email
 		if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 			if (mail ($to, $subject, $body, $headers)) {
-				$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+				$result='<div class="alert alert-success">Thank You! support team will contact you shortly</div>';
 			} else {
 				$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
 			}
